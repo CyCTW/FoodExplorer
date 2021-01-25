e59451c4b5c8dc363dfd9a1559a0cf9d8f9e6807
CyCTW <yuaannn_192.cs06@g2.nctu.edu.tw>
2021-01-25 18:59:05 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import {Key} from "./key"
+
+
+function App() {
+  
+  
+
+  // add lat and lng attribute in child component of GoogleMapReact
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+    Initial Commit
+    </div>
+  );
+}
+
+export default App;
